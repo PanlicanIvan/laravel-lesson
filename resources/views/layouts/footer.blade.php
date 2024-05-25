eb040a52a52e573a184abfc49f5beac306563b13
PanlicanIvan <ivandaxepanlican07@gmail.com>
2024-05-26 00:08:24 +0800

    Nice Admin Successfully Integrated!

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,10 @@
+<!-- ======= Footer ======= -->
+<footer id="footer" class="footer">
+    <div class="copyright">
+        &copy; Copyright <strong><span>Laravel Lesson</span></strong>. All Rights Reserved
+    </div>
+    <div class="credits">
+        Modified by <a href="">Ivan Daxe Panlican</a>
+    </div>
+</footer>
+<!-- End Footer -->
